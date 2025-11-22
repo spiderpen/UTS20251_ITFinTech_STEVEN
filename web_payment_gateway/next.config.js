de852662280a7f45d28512512011c92a999da201
Natania Faylinn <nataniafaylinn@gmail.com>
2025-11-22 19:02:16 +0700

    Update next.config.js

--- web_payment_gateway/next.config.js
+++ web_payment_gateway/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: [
+    "http://103.126.117.151:3000",
+  ],
 };
 
 export default nextConfig;
